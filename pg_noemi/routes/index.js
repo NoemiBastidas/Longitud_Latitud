1332b0083430a24415977b334653cfeb040adfda
Armandot1998 <armandotcajilema@gmail.com>
2020-01-23 11:36:30 -0500

    version1

---
+++ pg_noemi/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express'); // require Express
+var router = express.Router(); // setup usage of the Express router engine
+
+/* PostgreSQL and PostGIS module and connection setup */
+const { Client, Query } = require('pg')
+
+// Setup connection
+var username = "yourUsername" // sandbox username
+var password = "" // read only privileges on our table
+var host = "localhost:5432"
+var database = "Longitud" // database name
+var conString = "postgres://"+username+":"+password+"@"+host+"/"+database; // Your Database Connection
+
+// Set up your database query to display GeoJSON
+var coffee_query = "SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id, name)) As properties FROM coffee_shops As lg) As f) As fc";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
